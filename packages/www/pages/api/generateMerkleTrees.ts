69285cd89c839e8803cfd8c4c403f70bab8c8d61
Jonathan Cai <caimjonathan@gmail.com>
2022-07-06 04:18:11 -0400

    feat: generateMerkleTrees endpoint fetches submissions

--- packages/www/pages/api/generateMerkleTrees.ts
+++ packages/www/pages/api/generateMerkleTrees.ts
@@ -1,8 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import client from '../../backend/graphql-client'
+import { GET_SUBMISSIONS } from '../../graphql/queries'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { data } = await client.query({
+    query: GET_SUBMISSIONS,
+  })
+
   try {
-    res.status(200).json({ hehe: 'haha' })
+    res.status(200).json({ data })
   } catch (e) {
     console.log(e)
     res.status(500).json(e)
