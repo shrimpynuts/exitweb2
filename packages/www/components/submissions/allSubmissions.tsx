23df3daf01cb823bec035807bef7211ce57ed2e8
Jonathan Cai <caimjonathan@gmail.com>
2022-07-05 23:58:51 -0400

    feat: modularize create submission

--- packages/www/components/submissions/allSubmissions.tsx
+++ packages/www/components/submissions/allSubmissions.tsx
@@ -1,30 +1,17 @@
+import { useQuery } from '@apollo/client'
+
 import { GET_SUBMISSIONS } from '../../graphql/queries'
-import { useMutation, useQuery } from '@apollo/client'
 import { ISubmission } from '../../types'
 import Submission from './submission'
-import Button from '../util/button'
-import { INSERT_SUBMISSION_ONE } from '../../graphql/mutations'
+import CreateSubmission from './createSubmission'
 
 interface IProps {}
 
 export default function AllSubmissions({}: IProps) {
   const { data, loading } = useQuery(GET_SUBMISSIONS)
-  const [insertSubmission] = useMutation(INSERT_SUBMISSION_ONE)
-
-  const onClick = () => {
-    insertSubmission({
-      variables: {
-        submission: {
-          commitment: '33333',
-          community_id: 'b28923c8-507c-4dc0-80a0-baf45829ba9d',
-          proof_of_interaction: 'proof_of_interaction',
-        },
-      },
-    })
-  }
 
   return (
-    <div className="w-96">
+    <div className="flex">
       <div className="m-4 flex flex-col space-y-2">
         {data &&
           !loading &&
@@ -32,7 +19,9 @@ export default function AllSubmissions({}: IProps) {
           data.submissions.map((submission: ISubmission, idx: number) => (
             <Submission submission={submission} key={idx} />
           ))}
-        <Button onClick={onClick}>Add Submission</Button>
+      </div>
+      <div>
+        <CreateSubmission />
       </div>
     </div>
   )
---
+++ packages/www/components/submissions/createSubmission.tsx
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+import { INSERT_SUBMISSION_ONE } from '../../graphql/mutations'
+import { GET_SUBMISSIONS } from '../../graphql/queries'
+import { ISubmission } from '../../types'
+import Button from '../util/button'
+import toast from 'react-hot-toast'
+
+interface IProps {}
+
+interface IState {
+  commitment: string
+  community_id: string
+  proof_of_interaction: string
+}
+
+export default function CreateSubmission({}: IProps) {
+  const [insertSubmission] = useMutation(INSERT_SUBMISSION_ONE)
+  const [formState, setFormState] = useState<IState>({
+    commitment: '33333',
+    community_id: 'b28923c8-507c-4dc0-80a0-baf45829ba9d',
+    proof_of_interaction: 'proof_of_interaction',
+  })
+
+  const handleChange = (event: React.BaseSyntheticEvent) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const onClick = () => {
+    insertSubmission({
+      variables: {
+        submission: { ...formState },
+      },
+    }).then((res) => {
+      console.log({ res })
+      toast.success('Inserted new submission')
+    })
+  }
+
+  return (
+    <div className="">
+      <div className="flex flex-col mb-2">
+        <label>Commitment</label>
+        <input required type="text" name="commitment" value={formState.commitment} onChange={handleChange} />
+
+        <label>Community ID</label>
+        <input required type="text" name="community_id" value={formState.community_id} onChange={handleChange} />
+
+        <label>Proof Of Interaction</label>
+        <input
+          required
+          type="text"
+          name="proof_of_interaction"
+          value={formState.proof_of_interaction}
+          onChange={handleChange}
+        />
+      </div>
+
+      <Button onClick={onClick}>Add Submission</Button>
+    </div>
+  )
+}
