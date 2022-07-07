fd5030694f29463f2dd881675d61132ca120c3b1
Jonathan Cai <caimjonathan@gmail.com>
2022-07-07 18:02:53 -0400

    feat: single submission styling

--- packages/www/components/submissions/submission.tsx
+++ packages/www/components/submissions/submission.tsx
@@ -8,13 +8,12 @@ interface IProps {
 export default function AllSubmissions({ submission }: IProps) {
   return (
     <div className="p-3 border border-gray-200 shadow rounded">
-      <div>{submission.id}</div>
-      <div>
-        Created <Moment fromNow>{submission.created_at}</Moment>
-      </div>
+      <div className="">#{submission.id}</div>
       <div>{submission.commitment}</div>
       <div>{submission.proof_of_interaction}</div>
-      <div>{submission.community_id}</div>
+      <div className="text-sm text-gray-500 italic mt-1">
+        <Moment fromNow>{submission.created_at}</Moment>
+      </div>
     </div>
   )
 }
