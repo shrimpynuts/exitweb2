679ff0ebbb0daeb97c7aaab24fafa2229946e0c8
Jonathan Cai <caimjonathan@gmail.com>
2022-07-07 23:28:05 -0400

    feat: submission styling

--- packages/www/components/submissions/submission.tsx
+++ packages/www/components/submissions/submission.tsx
@@ -7,12 +7,16 @@ interface IProps {
 
 export default function AllSubmissions({ submission }: IProps) {
   return (
-    <div className="p-3 border border-gray-200 shadow rounded">
-      <div className="">#{submission.id}</div>
-      <div>{submission.commitment}</div>
-      <div>{submission.proof_of_interaction}</div>
-      <div className="text-sm text-gray-500 italic mt-1">
-        <Moment fromNow>{submission.created_at}</Moment>
+    <div className="p-3 border border-gray-200 shadow rounded flex justify-between space-x-8">
+      <div>
+        <div className="">#{submission.id}</div>
+        <div className="text-sm text-gray-500 italic mt-1">
+          <Moment fromNow>{submission.created_at}</Moment>
+        </div>
+      </div>
+      <div className="text-right">
+        <div>{submission.commitment}</div>
+        <div className="w-64 truncate">{submission.proof_of_interaction}</div>
       </div>
     </div>
   )
