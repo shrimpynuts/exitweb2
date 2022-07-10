3000c7e6f8297f96032547f064fd6db8782012e1
Jonathan Cai <caimjonathan@gmail.com>
2022-07-10 19:39:55 -0400

    feat: createSubmission creates and stores secret

--- packages/www/components/submissions/createSubmission.tsx
+++ packages/www/components/submissions/createSubmission.tsx
@@ -1,83 +1,70 @@
 import { useState } from 'react'
+
 import { useMutation } from '@apollo/client'
+import toast from 'react-hot-toast'
 
 import { INSERT_SUBMISSION_ONE } from '../../graphql/mutations'
+import { pedersenHashConcat, randomBigInt, toHex } from '../../lib/util'
 import Button from '../util/button'
-import toast from 'react-hot-toast'
 
 interface IProps {}
 
 interface IState {
-  commitment: string
   community_id: string
   proof_of_interaction: string
 }
 
+const DEFAULT_COMMUNITY_ID = 'f267857e-6840-49d7-a319-0a21a67114f3'
+const DEFAULT_COMMUNITY_HASH = '0x00ac9ebcfd1af25d43ab918d740e418a16f373e2ad94fbf5b5737c73576cac51'
+
 export default function CreateSubmission({}: IProps) {
   const [insertSubmission] = useMutation(INSERT_SUBMISSION_ONE)
+
   const [formState, setFormState] = useState<IState>({
-    commitment: 'default_commitment',
     proof_of_interaction: 'http://twitter.com/',
-    community_id: 'b28923c8-507c-4dc0-80a0-baf45829ba9d',
+    community_id: DEFAULT_COMMUNITY_ID,
   })
 
-  const handleChange = (event: React.BaseSyntheticEvent) => {
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value,
-    })
-  }
+  const handleChange = (event: React.BaseSyntheticEvent) =>
+    setFormState({ ...formState, [event.target.name]: event.target.value })
 
-  const onClick = () => {
-    insertSubmission({
-      variables: {
-        submission: { ...formState },
-      },
-    })
+  const onClick = async () => {
+    // Generate secret and store into localStorage under "DEFAULT_COMMUNITY_ID"
+    const secretKey = randomBigInt(31)
+    localStorage.setItem(`exitweb2/community/${DEFAULT_COMMUNITY_ID}`, toHex(secretKey))
+
+    const nullifier = BigInt(DEFAULT_COMMUNITY_HASH)
+
+    // Calculate commitment and create the submission object
+    const commitment = toHex(pedersenHashConcat(nullifier, secretKey))
+    const submission = { ...formState, commitment }
+
+    // Insert submission into database
+    insertSubmission({ variables: { submission } })
+      // Handle successful response
       .then((res) => {
         console.log({ res })
         toast.success('Created new submission.')
       })
+      // Handle error response
       .catch((err) => {
         toast.error(`Failed to create submission! ${err}`)
       })
   }
-  const handleFocus = (event: any) => event.target.select()
 
   return (
     <div className="flex flex-col w-96 mx-auto p-4 border border-gray-300 rounded">
-      {/* <label>Commitment</label>
-      <input
-        className="rounded border mb-2 border-gray-300"
-        required
-        type="text"
-        name="commitment"
-        value={formState.commitment}
-        onChange={handleChange}
-      /> */}
-
       <label className="text-xl font-bold">Join the community.</label>
       <p className="mt-2">Provide a link that proves that you belong in this community.</p>
       <input
         className="rounded border mt-2 mb-2 border-gray-300"
         required
         type="text"
-        onFocus={handleFocus}
+        onFocus={(event: any) => event.target.select()}
         name="proof_of_interaction"
         value={formState.proof_of_interaction}
         onChange={handleChange}
       />
-
-      {/* <label>Community ID (only one for now)</label>
-      <input
-        className="rounded border mb-2 border-gray-300 bg-gray-100 cursor-not-allowed"
-        required
-        disabled
-        type="text"
-        name="community_id"
-        value={formState.community_id}
-        onChange={handleChange}
-      /> */}
       <Button onClick={onClick}>Submit</Button>
     </div>
   )
--- packages/www/lib/util.ts
+++ packages/www/lib/util.ts
@@ -1,9 +1,44 @@
-export function addressIsAdmin(address: string) {
-  const adminAccounts = ['0xf725a0353dbB6aAd2a4692D49DDa0bE241f45fD0', '0xd6CB70a88bB0D8fB1be377bD3E48e603528AdB54']
-  return adminAccounts.includes(address)
+const circomlibjs = require('circomlibjs')
+import * as crypto from 'crypto'
+
+export function pedersenHash(nullifier: BigInt): BigInt {
+  return pedersenHashBuff(toBufferLE(nullifier as any, 31))
 }
 
-export function getServer() {
-  const domain = window.location.href.split('/').slice(0, 3).join('/')
-  return domain
+export function pedersenHashConcat(nullifier: BigInt, secret: BigInt): BigInt {
+  let nullBuff = toBufferLE(nullifier as any, 31)
+  let secBuff = toBufferLE(secret as any, 31)
+  let combinedBuffer = Buffer.concat([nullBuff, secBuff])
+  return pedersenHashBuff(combinedBuffer)
+}
+
+export function toHex(number: BigInt, length = 32) {
+  const str: string = number.toString(16)
+  return '0x' + str.padStart(length * 2, '0')
+}
+
+export function randomBigInt(nBytes: number): BigInt {
+  return toBigIntLE(crypto.randomBytes(nBytes))
+}
+
+export function toBigIntLE(buff: Buffer) {
+  const reversed = Buffer.from(buff)
+  reversed.reverse()
+  const hex = reversed.toString('hex')
+  if (hex.length === 0) {
+    return BigInt(0)
+  }
+  return BigInt(`0x${hex}`)
+}
+
+function pedersenHashBuff(buff: Buffer): BigInt {
+  let point = circomlibjs.pedersenHash.hash(buff)
+  return circomlibjs.babyjub.unpackPoint(point)[0]
+}
+
+function toBufferLE(bi: BigInt, width: number): Buffer {
+  const hex = bi.toString(16)
+  const buffer = Buffer.from(hex.padStart(width * 2, '0').slice(0, width * 2), 'hex')
+  buffer.reverse()
+  return buffer
 }
