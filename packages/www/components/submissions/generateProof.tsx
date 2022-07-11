abb6e33bb0b1632f46903fb1e6548719468a6f2a
Jonathan Cai <caimjonathan@gmail.com>
2022-07-11 13:58:00 -0400

    feat: proof generation loading spinner

--- packages/www/components/submissions/generateProof.tsx
+++ packages/www/components/submissions/generateProof.tsx
@@ -16,33 +16,42 @@ export default function GenerateProof({ community }: IProps) {
   const [proof, setProof] = useState<string>()
 
   const onGenerateProof = async () => {
-    const secretKey = localStorage.getItem(`exitweb2/community/${community.id}`)
-    if (!secretKey) return toast.error('No secret key found!')
+    try {
+      const secretKey = localStorage.getItem(`exitweb2/community/${community.id}`)
+      if (!secretKey) return toast.error('No secret key found!')
 
-    const communityHash = BigInt(community.hash)
-    const userSecret = BigInt(secretKey)
-    const computedCommitment = toHex(pedersenHashConcat(communityHash, userSecret))
+      const communityHash = BigInt(community.hash)
+      const userSecret = BigInt(secretKey)
+      const computedCommitment = toHex(pedersenHashConcat(communityHash, userSecret))
 
-    let domain = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://exitweb2.vercel.app'
-    let wasmBuff = await getFileBuffer(`${domain}/circuit.wasm`)
-    let zkeyBuff = await getFileBuffer(`${domain}/circuit_final.zkey`)
+      let domain = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://exitweb2.vercel.app'
+      let wasmBuff = await getFileBuffer(`${domain}/circuit.wasm`)
+      let zkeyBuff = await getFileBuffer(`${domain}/circuit_final.zkey`)
 
-    const address = '0xdef6919d69b28394B9D97FAf31b0CC2f5739ab57'
+      const address = '0xdef6919d69b28394B9D97FAf31b0CC2f5739ab57'
+      const merkleTree = MerkleTree.createFromStorageString(community.merkle_tree)
+      if (!merkleTree.leafExists(BigInt(computedCommitment))) return toast.error('Commitment not found in merkle tree!')
 
-    const merkleTree = MerkleTree.createFromStorageString(community.merkle_tree)
+      return generateProofCallData(merkleTree, communityHash, userSecret, address, wasmBuff, zkeyBuff)
+        .then(setProof)
+        .catch((err) => toast.error(err.message))
+    } catch (err: any) {
+      toast.error(err.message)
+    }
+  }
 
-    if (!merkleTree.leafExists(BigInt(computedCommitment))) return toast.error('Commitment not found in merkle tree!')
-
-    generateProofCallData(merkleTree, communityHash, userSecret, address, wasmBuff, zkeyBuff)
-      .then(setProof)
-      .then(() => toast.success('Proof generated'))
-      .catch((err) => toast.error(err.message))
+  const onClick = () => {
+    toast.promise(onGenerateProof(), {
+      loading: 'Generating proof...',
+      success: 'Proof successfully generated!',
+      error: (err) => err.toString(),
+    })
   }
 
   return (
     <div className="flex flex-col w-96 mx-auto p-4 border border-gray-300 rounded">
       <label className="text-xl font-bold">Generate a proof.</label>
-      <Button bgColor="bg-blue-600" classOverrides="mt-2" onClick={onGenerateProof}>
+      <Button bgColor="bg-blue-600" classOverrides="mt-2" onClick={onClick}>
         Generate Proof
       </Button>
       {proof && <CopyCode text={proof} />}
