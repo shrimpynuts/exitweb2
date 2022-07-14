561d24d82dbe534fc35317e6bf292e9d8e77997c
Jonathan Cai <caimjonathan@gmail.com>
2022-07-14 05:01:54 -0400

    feat: refactor generate proof

--- packages/www/components/submissions/generateProof.tsx
+++ packages/www/components/submissions/generateProof.tsx
@@ -19,25 +19,31 @@ export default function GenerateProof({ community, secretKey }: IProps) {
   const onGenerateProof = async () => {
     if (!community) return
     try {
-      if (!community.merkle_tree) {
-        return Promise.reject(new Error('Community has not generated a merkle tree yet!'))
-      }
+      if (!community.merkle_tree) return Promise.reject(new Error('Community has not generated a merkle tree yet!'))
 
       const communityHash = BigInt(community.hash)
       const userSecret = BigInt(secretKey)
       const computedCommitment = toHex(pedersenHashConcat(communityHash, userSecret))
 
+      // Fetch wasm and zkey
       let domain = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://exitweb2.vercel.app'
       let wasmBuff = await getFileBuffer(`${domain}/circuit.wasm`)
       let zkeyBuff = await getFileBuffer(`${domain}/circuit_final.zkey`)
 
-      const address = '0xdef6919d69b28394B9D97FAf31b0CC2f5739ab57'
+      // Create merkle tree object
       const merkleTree = MerkleTree.createFromStorageString(community.merkle_tree)
-      if (!merkleTree.leafExists(BigInt(computedCommitment))) return toast.error('Commitment not found in merkle tree!')
 
+      // Check if leaf is even in the merkle tree
+      if (!merkleTree.leafExists(BigInt(computedCommitment)))
+        return Promise.reject(new Error('Commitment not found in merkle tree!'))
+
+      // TODO: require user sign in w/ Ethereum to generate proof and claim token
+      const address = '0xdef6919d69b28394B9D97FAf31b0CC2f5739ab57'
+
+      // Generate the proof
       return generateProofCallData(merkleTree, communityHash, userSecret, address, wasmBuff, zkeyBuff)
         .then(setProof)
-        .catch((err) => toast.error(err.message))
+        .catch(Promise.reject)
     } catch (err: any) {
       toast.error(err.message)
     }
