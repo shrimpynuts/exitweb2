56bdbc4e25069d572bb3a7983ef4023de264bb30
Jonathan Cai <caimjonathan@gmail.com>
2022-07-14 01:17:34 -0400

    feat: cleanup home index

--- packages/www/components/home/index.tsx
+++ packages/www/components/home/index.tsx
@@ -1,16 +1,8 @@
-import { useQuery } from '@apollo/client'
 import { useRouter } from 'next/dist/client/router'
-import { GET_ALL_COMMUNITIES } from '../../graphql/queries'
-import { ICommunity } from '../../types'
-import CreateSubmission from '../submissions/createSubmission'
-import GenerateProof from '../submissions/generateProof'
 import Button from '../util/button'
 
 export default function Home() {
-  const { data } = useQuery(GET_ALL_COMMUNITIES)
   const router = useRouter()
-
-  const communities: ICommunity[] = data?.community
   return (
     <div className="mt-8 sm:w-3/4 lg:w-2/3 mx-auto">
       <div className="flex flex-col space-y-8 mx-4 lg:mx-auto lg:w-96">
@@ -39,12 +31,6 @@ export default function Home() {
           </Button>
         </div>
       </div>
-      {communities && communities[0] && (
-        <div className="flex flex-col space-y-4">
-          <CreateSubmission community={communities[0]} />
-          <GenerateProof community={communities[0]} />
-        </div>
-      )}
     </div>
   )
 }
