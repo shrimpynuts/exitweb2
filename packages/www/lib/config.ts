56ba767c0323d4d05234ef9e77672a734b685748
Jonathan Cai <caimjonathan@gmail.com>
2022-07-21 17:59:15 -0400

    test: goerli contract test

--- packages/www/lib/config.ts
+++ packages/www/lib/config.ts
@@ -3,7 +3,7 @@ import PRIVATE_AIRDROP_JSON from './ABIs/PrivateAirdrop.json'
 import COMMUNITY_TOKEN_JSON from './ABIs/CommunityToken.json'
 
 /** Private airdrop contract configs */
-export const AIRDROP_CONTRACT_ADDRESS = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512'
+export const AIRDROP_CONTRACT_ADDRESS = '0x8C579c60Da3b0d767Da80cb3FC728b9d964E195B'
 export const AIRDROP_ABI = PRIVATE_AIRDROP_JSON.abi
 
 export const AIRDROP_CONTRACT_DATA = {
