98a4700d30d66466746c19956753d3e612dbc3bb
Jonathan Cai <caimjonathan@gmail.com>
2022-08-01 14:32:04 -0400

    feat: index bg colors

--- packages/www/pages/index.tsx
+++ packages/www/pages/index.tsx
@@ -29,27 +29,27 @@ export default function HomePage({ communities }: IProps) {
         <Toaster position="top-center" reverseOrder={false} />
 
         {/* Hero section */}
-        <section className="bg-blue-100 h-full">
+        <section className="bg-gray-100 h-full">
           <div className="max-w-screen-xl m-auto pb-4 md:pb-12">
             <div className="flex py-24 px-8 md:max-w-6xl mx-auto">
               {/* Left side */}
               <div className="flex flex-col">
                 <SlowShow step={1}>
-                  <h1 className="text-5xl font-extrabold text-blue-900 tracking-tight">Connect with your tribe.</h1>
+                  <h1 className="text-5xl font-extrabold text-gray-900 tracking-tight">Connect with your tribe.</h1>
                 </SlowShow>
                 <SlowShow step={2}>
-                  <h1 className="text-2xl font-semibold my-4 text-blue-900">A new pseudonymous social experience.</h1>
+                  <h1 className="text-2xl font-semibold my-4 text-gray-900">A new pseudonymous social experience.</h1>
                 </SlowShow>
 
                 <SlowShow step={3}>
                   <div className="flex space-x-2 my-2">
                     <Link href="/join-community">
-                      <Button classOverrides="py-1 text-lg text-blue-50 rounded-lg" bgColor="bg-black">
+                      <Button classOverrides="py-1 text-lg text-gray-50 rounded-lg" bgColor="bg-black">
                         Join a community
                       </Button>
                     </Link>
                     <Link href="/create-community">
-                      <Button classOverrides="py-1 text-lg text-blue-50 rounded-lg">Start a community</Button>
+                      <Button classOverrides="py-1 text-lg text-gray-50 rounded-lg">Start a community</Button>
                     </Link>
                   </div>
                 </SlowShow>
