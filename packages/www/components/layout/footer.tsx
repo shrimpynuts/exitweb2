d3b340402f1793fec367faab2fdbd186e4c27ebf
Jonathan Cai <caimjonathan@gmail.com>
2022-08-01 11:22:26 -0400

    feat: footer styling

--- packages/www/components/layout/footer.tsx
+++ packages/www/components/layout/footer.tsx
@@ -1,16 +1,22 @@
-function Footer() {
+import Link from 'next/link'
+
+export default function Footer() {
   return (
     <footer className="bg-gray-100 mt-8">
       <div className="container mx-auto px-8 md:px-24">
         <div className="w-full flex flex-row justify-between py-8">
           <div className="flex space-x-8">
-            <div>About</div>
-            <div>FAQ</div>
-            <div>Contact</div>
+            <Link href="/about" passHref>
+              <a className="text-gray-600 hover:text-gray-800">About</a>
+            </Link>
           </div>
           <div>
-            {/* <div>Twitter</div> */}
-            <a href="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <a
+              href="https://twitter.com/exitweb2"
+              className="text-gray-600 hover:text-gray-800 dark:hover:text-white"
+              rel="noreferrer"
+              target="_blank"
+            >
               <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
               </svg>
@@ -22,5 +28,3 @@ function Footer() {
     </footer>
   )
 }
-
-export default Footer
